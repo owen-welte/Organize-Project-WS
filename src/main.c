1925dfa662cfa7338e5c53b7497e50d27da90dcd
Curs3 W4ll <corentin.hervaud@epitech.eu>
2021-01-12 11:14:27 +0100

    {FILE} [Modif] Move lib functions, add algo

--- src/main.c
+++ src/main.c
@@ -6,46 +6,85 @@
 */
 
 #include "project.h"
+#include "lib.h"
 
-int my_strlen(const char *str)
-{
-    int count = 0;
-
-    if (!str)
-        return -1;
-    while (str[count])
-        count++;
-    return count;
-}
-
-void my_putstr(char const *str)
-{
-    write(1, str, my_strlen(str));
-}
-
-void my_putchar(char const c)
-{
-    write(1, &c, 1);
-}
-
-int print_error(char const *error_message)
+void print_error(char const *error_message)
 {
     write(2, error_message, my_strlen(error_message));
-    return 84;
+}
+
+int check_error(int ac)
+{
+    if (ac != 3) {
+        if (ac > 3) {
+            print_error(TOO_MANY_ARGS);
+            return 1;
+        } else if (ac < 3) {
+            print_error(TOO_FEW_ARGS);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int detect_number(const char *arg)
+{
+    for (int i = 0; arg[i]; i++)
+        if (arg[i] < '0' || arg[i] > '9')
+            return 0;
+    my_putstr("Number)");
+    return 1;
+}
+
+int detect_lowcase(const char *arg)
+{
+    for (int i = 0; arg[i]; i++)
+        if (arg[i] < 'a' || arg[i] > 'z')
+            return 0;
+    my_putstr("Lowcase word)");
+    return 1;
+}
+
+int detect_upcase(const char *arg)
+{
+    for (int i = 0; arg[i]; i++)
+        if (arg[i] < 'A' || arg[i] > 'Z')
+            return 0;
+    my_putstr("Upcase word)");
+    return 1;
+}
+
+int detect_word(const char *arg)
+{
+    for (int i = 0; arg[i]; i++)
+        if ((arg[i] < 'A' || arg[i] > 'Z') && (arg[i] < 'a' || arg[i] > 'z'))
+            return 0;
+    my_putstr("Word)");
+    return 1;
+}
+
+int detect_arg_type(const char *arg)
+{
+    my_putstr(" (");
+    if (detect_number(arg) == 1)
+        return 0;
+    else if (detect_lowcase(arg) == 1 || detect_upcase(arg) == 1)
+        return 0;
+    else if (detect_word(arg) == 1)
+        return 0;
+    else
+        my_putstr("String)");
+    return 1;
 }
 
 int main(int ac, char const *av[])
 {
-    if (ac != 3) {
-        if (ac > 3)
-            return print_error(TOO_MANY_ARGS);
-        if (ac < 3)
-            return print_error(TOO_FEW_ARGS);
-    }
-
+    if (check_error(ac) == 1)
+        return 84;
     for (int i = 0; i < ac; i++) {
         my_putstr(av[i]);
-        my_putchar(' ');
+        detect_arg_type(av[i]);
+        my_putchar('\n');
     }
     return 0;
 }
